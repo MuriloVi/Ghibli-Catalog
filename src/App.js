db425331958d3d4f9fdd22576ee4900e5242d45c
MuriloVi <murilovif17@gmail.com>
2020-07-28 13:44:47 -0300

    Início do projeto

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//code use
+import { useEffect, useState } from 'react'
+import { getAllFilms } from './services/ghibli';
+
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [ghibliFilms, setGhiblifilms] = useState([]);
+  const baseURL = 'https://ghibliapi.herokuapp.com/films/';
+
+  useEffect(()=>{
+    async function fetchFilms(){
+      let response = await getAllFilms(baseURL)
+      console.log(response)
+      await loadingGhibli(response)
+      setLoading(false)
+    }
+    fetchFilms()
+  }, [])
+
+  const loadingGhibli = async (data) =>{
+    let ghibliData = await Promise.all(data.map(async film => film))
+    setGhiblifilms(ghibliData)
+   }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     {loading ? <h3 style={{textAlign:"center"}}>Loading...</h3> : (
+       <>
+      <div className="Catalog">
+        {ghibliFilms.map((film) =>{
+          return <h2 key={film.id}>{film.title}</h2>
+        })}
+      </div>
+       </>
+
+     )}
+    </div>  
   );
 }
 
